dccb9e434d8aa7372ca9d34f88ba9b181b94ea3d
Dominic Morgan <dominic.mark.morgan@gmail.com>
2019-10-18 14:22:58 +0800

    Update watch script so live reloading works

--- assets/watch.sh
+++ assets/watch.sh
@@ -2,4 +2,4 @@ STATIC_DIR="../priv/static"
 if [ -d "$STATIC_DIR" ]; then rm -Rf "$STATIC_DIR"; fi
 mkdir "$STATIC_DIR"
 cp -R static/* "$STATIC_DIR"
-parcel build src/index.js --out-dir "$STATIC_DIR/dist" --public-url dist 
+parcel src/index.js --out-dir "$STATIC_DIR/dist" --public-url dist 
