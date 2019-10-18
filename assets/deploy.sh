e7d8ef82f2fe9c048a1a8b26072f2f4f524ea2cc
Dominic Morgan <dominic.mark.morgan@gmail.com>
2019-10-18 11:39:31 +0800

    create static folder before deploy and watch

---
+++ assets/deploy.sh
@@ -0,0 +1,5 @@
+STATIC_DIR="../priv/static"
+if [ -d "$STATIC_DIR" ]; then rm -Rf $STATIC_DIR; fi
+mkdir "$STATIC_DIR"
+cp -R static/* $STATIC_DIR
+parcel build src/index.js --out-dir "$STATIC_DIR/dist" --public-url dist  --no-cache
